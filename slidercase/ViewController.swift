f091afd0c9cabaef9c128b0c6e3264ad9e7a7d06
brianke <oooreo612@gmail.com>
2023-01-31 10:34:02 +0800

    Initial Commit

---
+++ slidercase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  slidercase
+//
+//  Created by iroda on 2023/1/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
